473dbbce67311ffb3f469d0e5647c3ca3231b713
Hiu-626 <onlywater96@gmail.com>
2025-12-17 21:28:45 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ⚠️ 下面這一行好重要，如果無，GitHub Pages 會搵唔到 CSS/JS
+  base: '/Osaka/', 
 })
