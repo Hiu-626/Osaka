f32f1ef0b286f0036f86b6d03dea194854cd8567
Hiu-626 <onlywater96@gmail.com>
2025-12-18 00:16:14 +1100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,10 @@
-import React, { useState } from "react";
-// Removed Firebase Auth imports as we are using LocalStorage for the GitHub version
-import { Tab } from "./types";
-import { BottomNav } from "./components/BottomNav";
-import { Schedule } from "./views/Schedule";
-import { Bookings } from "./views/Bookings";
-import { Expense } from "./views/Expense";
-import { Journal } from "./views/Journal";
-import { Planning } from "./views/Planning";
-import { Members } from "./views/Members";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // 可有可無
 
-function App() {
-  const [currentTab, setCurrentTab] = useState<Tab>(Tab.Schedule);
-  
-  // No auth loading state needed for LocalStorage version
-  const renderView = () => {
-    switch (currentTab) {
-      case Tab.Schedule: return <Schedule />;
-      case Tab.Bookings: return <Bookings />;
-      case Tab.Expense: return <Expense />;
-      case Tab.Journal: return <Journal />;
-      case Tab.Planning: return <Planning />;
-      case Tab.Members: return <Members />;
-      default: return <Schedule />;
-    }
-  };
-
-  return (
-    <div className="h-[100dvh] w-full bg-[#E0F2FE] relative overflow-hidden flex flex-col font-sans text-gray-800 bg-[radial-gradient(#60A5FA_1px,transparent_1px)] [background-size:20px_20px]">
-      <div className="flex-1 overflow-hidden relative">
-        {renderView()}
-      </div>
-      <BottomNav currentTab={currentTab} onTabChange={setCurrentTab} />
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
