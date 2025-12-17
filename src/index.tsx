da92985323241c8a45a525d471a95b834fb8b48f
Hiu-626 <onlywater96@gmail.com>
2025-12-18 00:19:02 +1100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // 可有可無
+// import "./index.css";  <-- 暫時屏蔽左佢，免得報錯
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
