17da67d9d090bb3aced422414bccd1a71a80292a
Hiu-626 <onlywater96@gmail.com>
2025-12-17 21:37:17 +1100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,20 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+    const env = loadEnv(mode, process.cwd(), '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
+      // ⚠️ 下面呢行最重要！唔改會白畫面！
+      base: '/你的-repo-名/', 
+      
       plugins: [react()],
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'), // 通常指去 src folder
+        }
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
       }
     };
 });
